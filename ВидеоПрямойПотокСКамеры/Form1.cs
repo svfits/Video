8160c2cfe76de848fb806f3db4e4cf83a2d900c4
svfits <a1d1@inbox.ru>
2016-03-28 16:29:44 +0800

    видео поток с камеры

---
+++ ВидеоПрямойПотокСКамеры/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ВидеоПрямойПотокСКамеры
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                axVLCPlugin21.playlist.add("rtsp://SuAP:17GhbtvGhbtv@192.168.102.99:554/live1.sdp");
+                axVLCPlugin21.playlist.play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ой что то произошло :( перезапустите приложение  " + ex);
+            }
+        }
+    }
+}
